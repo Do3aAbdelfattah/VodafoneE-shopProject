c00df46d53f9819985898eeef665d2b14c4412c4
do3aAbdelfattah <doaaabdelfattah94@gmail.com>
2024-02-01 23:39:30 +0200

    Extent Report Added

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "steps"
+        glue = "steps",
+        plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestRunner extends TestBase {
 }
